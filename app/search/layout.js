ef7337c710579871f925bd18d9e38eddfad14cd8
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-07-28 18:37:49 +0200

    fixed the footer, added menu

---
+++ app/search/layout.js
@@ -0,0 +1,15 @@
+import Footer from '@/components/layout/footer';
+
+
+export default function SearchLayout({ children }) {
+  return (
+    <>
+      <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row dark:text-white">
+       
+        <div className="order-last min-h-screen w-full md:order-none">{children}</div>
+        
+      </div>
+      <Footer />
+    </>
+  );
+}
--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -2,15 +2,38 @@ import Link from 'next/link';
 
 import LogoSquare from '@/components/logo-square';
 import { Suspense } from 'react';
+import FooterMenu from './footer-menu';
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
-  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
+  const copyrightDate = 2024 + (currentYear > 2024 ? `-${currentYear}` : '');
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
+  const menu = [
+    {
+      title: 'Home',
+      path: '/'
+    },
+    {
+      title: 'About',
+      path: '/about'
+    },
+    {
+      title: 'Contact',
+      path: '/contact'
+    },
+    {
+      title: 'privacy-and-policy',
+      path: '/privacy-and-policy'
+    },
+    {
+      title: 'Store',
+      path: '/store'
+    },
+  ];
   return (
     <footer className="text-sm text-neutral-500 dark:text-neutral-400">
       <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-700">
@@ -32,18 +55,9 @@ export default function Footer() {
             </div>
           }
         >
+          <FooterMenu menu={menu} />
         </Suspense>
-        <div className="md:ml-auto">
-          <a
-            className="flex h-8 w-max flex-none items-center justify-center rounded-md border border-neutral-200 bg-white text-xs text-black dark:border-neutral-700 dark:bg-black dark:text-white"
-            aria-label="Deploy on Vercel"
-            href="https://vercel.com/templates/next.js/nextjs-commerce"
-          >
-            <span className="px-3">▲</span>
-            <hr className="h-full border-r border-neutral-200 dark:border-neutral-700" />
-            <span className="px-3">Deploy</span>
-          </a>
-        </div>
+        
       </div>
       <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
