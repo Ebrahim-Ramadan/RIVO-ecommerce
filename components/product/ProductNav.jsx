f125e56b0e76a0a51949fc3102ab4dde11f90347
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-07-30 18:09:50 +0200

    fixed the hover

--- components/product/ProductNav.jsx
+++ components/product/ProductNav.jsx
@@ -6,12 +6,12 @@ import { toast } from 'react-hot-toast';
 
 export const ProductNav = ({FrameId}) => {
   return (
-    <div className='w-full flex flex-row justify-between py-4 items-center'>
-            <a href='/' className='rounded-full p-2 bg-blue-600 text-white'>
+    <div className='w-full flex flex-row justify-between py-4 items-center [&>*]:transition [&>*]:duration-200 '>
+            <a href='/' className='rounded-full p-2 bg-blue-600 text-white  hover:bg-blue-700'>
             <ArrowLeft size='16'/>
 
             </a>
-            <button href='/' className='rounded-full p-2 bg-blue-600 text-white' onClick={()=>{
+            <button href='/' className='hover:bg-blue-700 rounded-full p-2 bg-blue-600 text-white' onClick={()=>{
               copyToClipboard(`${process.env.NEXT_PUBLIC_API_URL}/frame/${FrameId}`)
               toast.success('Link Copied to clipboard')
             }}>
