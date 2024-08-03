9a51d220cf62eac62d4a18afcb29cbb60db6eba6
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-08-03 21:37:38 +0200

    fixed the bg white

--- app/layout.js
+++ app/layout.js
@@ -64,7 +64,7 @@ export default async function RootLayout({ children }) {
 
   return (
        <html lang="en" className={GeistSans.variable}>
-      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+      <body className="bg-black text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
       <Toaster position="top-center" />
         <Navbar />
         <main>{children}</main>
--- components/cart/modal.jsx
+++ components/cart/modal.jsx
@@ -111,7 +111,7 @@ export default function CartModal() {
                 <div className="mt-20  gap-4 flex w-full flex-col items-center justify-center overflow-hidden">
                   <ShoppingBag className="h-16" />
                   <p className="mt-6 text-center text-xl font-medium">Your cart is empty.</p>
-                  <a href='/' className='text-center font-bold md:text-xl w-full px-4 py-2 bg-white/20 hover:bg-white/10 transition duration-200 backdrop-blur-[.5px] rounded-full '>Back to Shop</a>
+                  <button onClick={closeCart} className='text-center font-bold md:text-xl w-full px-4 py-2 bg-white/20 hover:bg-white/10 transition duration-200 backdrop-blur-[.5px] rounded-full '>Back to Shop</button>
                 </div>
               ) : (
                 <div className="flex h-full flex-col justify-between overflow-hidden px-2">
