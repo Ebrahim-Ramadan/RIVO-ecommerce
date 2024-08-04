a85ab68dbfdc12e9621e905dc0cef93d5878d4ba
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-08-04 12:19:05 +0200

    added empty videos for wishlist and fixed the ui bug shadow

--- components/orders/OrdersLayout.jsx
+++ components/orders/OrdersLayout.jsx
@@ -95,7 +95,7 @@ export default function OrdersLayout({ newOrderID }) {
     <div className="max-w-4xl mx-auto p-4 ">
     <a href="/" className="text-blue-600 hover:underline mb-4 inline-block">← Back to Shopping</a>
     
-    <h1 className="text-3xl font-bold  mb-2">Orders summary</h1>
+    <h1 className="text-3xl font-bold  mb-2">My Orders</h1>
     <p className=" mb-6">Find order invoice, payment and shipping details here</p>
     <div className="flex justify-end items-center ">
     <CancelOrder trigger={'Cancel Order'} />
@@ -108,7 +108,7 @@ export default function OrdersLayout({ newOrderID }) {
       
       return (
         <div key={orderId} className={`flex flex-col md:flex-row gap-6 border  p-4 rounded-lg ${newOrderID == orderId ? 'border-green-600' : 'border-white/10'}`}>
-          <div className=" rounded-lg shadow-md flex-grow">
+          <div className=" rounded-lg  flex-grow">
             <p className="text-green-500 flex flex-row items-center gap-2  font-semibold  mb-1">Successful Order ID 
             <span >{orderId} </span>
             </p>
@@ -151,7 +151,7 @@ export default function OrdersLayout({ newOrderID }) {
             
           </div>
           
-          <div className=" rounded-lg shadow-md w-full md:w-80">
+          <div className=" rounded-lg  w-full md:w-80">
             <h3 className="text-lg font-semibold  mb-4">Order invoice</h3>
             <div className="space-y-2 mb-4">
               {/* <div className="flex justify-between">
