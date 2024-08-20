0326d2c258199831ad8b73a17f8c7cf5d588b2ef
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-08-20 17:15:12 +0200

    removed the footer layout at /frame/[id]

--- app/frame/[id]/page.jsx
+++ app/frame/[id]/page.jsx
@@ -1,6 +1,4 @@
-import Footer from '@/components/layout/footer';
 import { ProductOverview } from '@/components/product/ProductOverview';
-import LazyLoad from '@/lib/LazyLoad';
 
 export const revalidate = 360000;
 
@@ -9,9 +7,7 @@ export default async function Page({params}) {
   return (
     <>
      <ProductOverview frameID = {params.id}/>
-     <LazyLoad>
-     <Footer/>
-     </LazyLoad>
+    
     </>
   );
 }
