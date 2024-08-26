bc01a70fef03d36326930c603afc73b2b3bdc8c3
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-08-26 22:24:59 +0200

    fixed privacy and policy route in footer

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -31,7 +31,7 @@ export  function Footer() {
     },
     {
       title: 'Privacy and Policy',
-      path: '/privacy-and-policy'
+      path: '/privacyandpolicy'
     },
     {
       title: 'Development',
