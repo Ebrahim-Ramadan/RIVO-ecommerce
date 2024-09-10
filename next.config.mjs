64ea616ee039c9c8f07b509976543eddf41a00cc
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-09-10 22:11:08 +0200

    fixed the dynamicIO experiemental feature for req caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental:{
-    // ppr:true,
-    dynamicIO : true,
-  },
     images: {
         remotePatterns: [
           {
